42b340a876e6c7bacec7c35eab72d7b3e0d4b175
K. Pickett <designintheblack@gmail.com>
2025-01-16 19:12:25 -0500

    Update color scheme documentation and type scale

--- elevate/core/system/design/colors.ts
+++ elevate/core/system/design/colors.ts
@@ -1,58 +1,116 @@
 export const colors = {
-    'purple-4': '#31063E',
-    'purple-3': '#3F084E',
-    'purple-2': '#4A095F',
-    'purple-1': '#6F10A0',
-    'purple': '#9B16D8',
-    'purple+1': '#BA1CFF',
-    'purple+2': '#D940FF',
-    'purple+3': '#E97AFF',
-    'purple+4': '#F4B3FF',
-    
-    'red-4': '#450F1A',
-    'red-3': '#561421',
-    'red-2': '#691727',
-    'red-1': '#A1203E',
-    'red': '#D82A54',
-    'red+1': '#F9426A',
-    'red+2': '#FF627F',
-    'red+3': '#FF8B9B',
-    'red+4': '#FFB5C2',
-    
-    'indigo-4': '#22253F',
-    'indigo-3': '#2B304D',
-    'indigo-2': '#363B5F',
-    'indigo-1': '#53589A',
-    'indigo': '#7075D3',
-    'indigo+1': '#8891FF',
-    'indigo+2': '#A3A9FF',
-    'indigo+3': '#BCC2FF',
-    'indigo+4': '#D6DCFF',
-    
-    'orange-4': '#4A2C17',
-    'orange-3': '#5B361E',
-    'orange-2': '#714222',
-    'orange-1': '#B56335',
-    'orange': '#F48247',
-    'orange+1': '#FF9A5B',
-    'orange+2': '#FFB07A',
-    'orange+3': '#FFC59A',
-    'orange+4': '#FFD9BB',
-    
-    'green-4': '#153A32',
-    'green-3': '#1A453C',
-    'green-2': '#1E5147',
-    'green-1': '#2C7E6F',
-    'green': '#3CAA96',
-    'green+1': '#4CD8BE',
-    'green+2': '#63F3E0',
-    'green+3': '#87FFF1',
-    'green+4': '#ACFFF8',
-    
 
-    'black': '#0C090D',
-   
+        // --- Existing Colors ---
+        // PURPLE
+        'purple-4': '#31063E',
+        'purple-3': '#3F084E',
+        'purple-2': '#4A095F',
+        'purple-1': '#6F10A0',
+        'purple':   '#9B16D8',
+        'purple+1': '#BA1CFF',
+        'purple+2': '#D940FF',
+        'purple+3': '#E97AFF',
+        'purple+4': '#F4B3FF',
+        
+        // RED
+        'red-4': '#450F1A',
+        'red-3': '#561421',
+        'red-2': '#691727',
+        'red-1': '#A1203E',
+        'red':   '#D82A54',
+        'red+1': '#F9426A',
+        'red+2': '#FF627F',
+        'red+3': '#FF8B9B',
+        'red+4': '#FFB5C2',
+        
+        // INDIGO
+        'indigo-4': '#22253F',
+        'indigo-3': '#2B304D',
+        'indigo-2': '#363B5F',
+        'indigo-1': '#53589A',
+        'indigo':   '#7075D3',
+        'indigo+1': '#8891FF',
+        'indigo+2': '#A3A9FF',
+        'indigo+3': '#BCC2FF',
+        'indigo+4': '#D6DCFF',
+        
+        // ORANGE
+        'orange-4': '#4A2C17',
+        'orange-3': '#5B361E',
+        'orange-2': '#714222',
+        'orange-1': '#B56335',
+        'orange':   '#F48247',
+        'orange+1': '#FF9A5B',
+        'orange+2': '#FFB07A',
+        'orange+3': '#FFC59A',
+        'orange+4': '#FFD9BB',
+        
+        // GREEN
+        'green-4': '#153A32',
+        'green-3': '#1A453C',
+        'green-2': '#1E5147',
+        'green-1': '#2C7E6F',
+        'green':   '#3CAA96',
+        'green+1': '#4CD8BE',
+        'green+2': '#63F3E0',
+        'green+3': '#87FFF1',
+        'green+4': '#ACFFF8',
+      
+        // --- Neutral/Black & White ---
+        // BLACK (expanded)
+
+        'black':   '#0C0C0C',
+
+        // GREY (newly added)
+        'grey-4': '#121212',
+        'grey-3': '#1F1F1F',
+        'grey-2': '#2E2E2E',
+        'grey-1': '#3C3C3C',
+        'grey':   '#5A5A5A',
+        'grey+1': '#7A7A7A',
+        'grey+2': '#9A9A9A',
+        'grey+3': '#BDBDBD',
+        'grey+4': '#DCDCDC',
+      
+        // WHITE (expanded)
+
+        'white':   '#FCFCFC',
+
+      
+        // --- Newly Added Colors ---
+        // BLUE
+        'blue-4': '#0E1E39',
+        'blue-3': '#0F2A4C',
+        'blue-2': '#103661',
+        'blue-1': '#13518F',
+        'blue':   '#1673F5',
+        'blue+1': '#3B90FF',
+        'blue+2': '#68ADFF',
+        'blue+3': '#99C8FF',
+        'blue+4': '#CCE4FF',
+      
+        // PINK
+        'pink-4': '#4D0124',
+        'pink-3': '#660131',
+        'pink-2': '#82013E',
+        'pink-1': '#B00A66',
+        'pink':   '#FF4FA7',
+        'pink+1': '#FF78BB',
+        'pink+2': '#FFA0CE',
+        'pink+3': '#FFC2DF',
+        'pink+4': '#FFE0F0',
+      
+        // YELLOW
+        'yellow-4': '#4A4000',
+        'yellow-3': '#645400',
+        'yellow-2': '#7F6800',
+        'yellow-1': '#B29800',
+        'yellow':   '#FFD600',
+        'yellow+1': '#FFEE4D',
+        'yellow+2': '#FFF375',
+        'yellow+3': '#FFF79E',
+        'yellow+4': '#FFFBCC',
+      
+
     
-    'white': '#FDFFFC',
-   
 }
